d2fe56577e0bf6031ec9a89469b2100b8c6b1e5d
czx <2274623566@qq.com>
2025-12-27 13:38:16 +0800

    后端: 用户管理功能

--- app/internal/inits/inits.go
+++ app/internal/inits/inits.go
@@ -4,10 +4,21 @@ import (
 	"Faber-AI/app/internal/router"
 
 	"github.com/mszlu521/thunder/config"
+	"github.com/mszlu521/thunder/database"
 	"github.com/mszlu521/thunder/server"
+	"github.com/mszlu521/thunder/tools/jwt"
 )
 
 func Init(s *server.Server, conf *config.Config) {
-
-	s.RegisterRouters(&router.Event{}, &router.AuthRouter{})
+	// 初始化数据库
+	database.InitPostgres(conf.DB.Postgres)
+	// 初始化 redis
+	database.InitRedis(conf.DB.Redis)
+	// 初始化 jwt
+	jwt.Init(conf.Jwt.GetSecret())
+	s.RegisterRouters(
+		&router.Event{},
+		&router.AuthRouter{},
+		&router.SubscriptionRouter{},
+	)
 }
--- app/internal/router/auths.go
+++ app/internal/router/auths.go
@@ -9,12 +9,16 @@ import (
 type AuthRouter struct {
 }
 
-// Register 负责注册用户相关的路由
 func (u *AuthRouter) Register(engine *gin.Engine) {
-	// 创建一个路由组
 	userGroup := engine.Group("/api/v1/auth")
 	{
 		userHandler := auths.NewHandler()
-		userGroup.GET("/register", userHandler.Register)
+		userGroup.POST("/register", userHandler.Register)
+		userGroup.GET("/verify-email", userHandler.VerifyEmail)
+		userGroup.POST("/login", userHandler.Login)
+		userGroup.POST("/refresh-token", userHandler.RefreshToken)
+		userGroup.POST("/forgot-password", userHandler.ForgotPassword)
+		userGroup.POST("/verify-code", userHandler.VerifyCode)
+		userGroup.POST("/reset-password", userHandler.ResetPassword)
 	}
 }
